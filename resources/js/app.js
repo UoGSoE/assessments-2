706ae0d59c6a87091158fcdca039c65261820d5e
Claudia Schroeder <claudia.schroeder@glasgow.ac.uk>
2025-06-11 14:34:26 +0100

    Set up initial setup

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+
+window.Calendar = Calendar;
+window.dayGridPlugin = dayGridPlugin;
+window.timeGridPlugin = timeGridPlugin;
+window.listPlugin = listPlugin;
